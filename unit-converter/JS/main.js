58076d3f18ee7d8c8458f98c832a0a32136fa4a0
William Dela Rosa <williamgdelarosa1220@gmail.com>
2023-09-08 21:27:33 +0200

    style: Adjusted colors

---
+++ unit-converter/JS/main.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector(".input-field");
+const convertBtnEl = document.querySelector(".convert-btn");
+const dLeftLengthEl = document.querySelector(".d-left-length");
+const dRightLengthEl = document.querySelector(".d-right-length");
+const dLeftVolumeEl = document.querySelector(".d-left-volume");
+const dRightVolumeEl = document.querySelector(".d-right-volume");
+const dLeftMass = document.querySelector(".d-left-mass");
+const dRightMass = document.querySelector(".d-right-mass")
+
