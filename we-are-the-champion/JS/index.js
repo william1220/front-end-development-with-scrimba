8ecd295b5a5aaa2241c591f088269dfd9a610a0d
William Dela Rosa <williamgdelarosa1220@gmail.com>
2023-09-22 06:09:41 +0200

    style: Finished CSS styling and configured Firebase database

---
+++ we-are-the-champion/JS/index.js
@@ -0,0 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-cdbff-default-rtdb.europe-west1.firebasedatabase.app/"
+}
